643756cb50f8efa65c3a70ab19f2281ebcaefa29
George <grguilfo@uno.edu>
2022-03-22 13:47:52 -0500

    First Program

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+//George Guilford
+//Pushing code to github
+
+
+public class FirstProgram{
+	
+	public static String sayHello(){
+		return "Hello GitHub";
+	}
+
+	public static void main(String[] args){
+		System.out.println(sayHello());
+	}
+
+}
